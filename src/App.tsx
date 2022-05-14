f500828adc8ee632018da859f8f61dd70404aa58
rafaellevissa <rafaellevissa@gmail.com>
2022-05-14 19:12:15 -0300

    remove components

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { Provider as ReduxProvider } from "react-redux";
 import store from './states/store';
 
 import Login from './routes/login';
-import SignUP from './routes/signup';
-import Recovery from './pages/recovery';
 import Users from './routes/users';
 
 export default function App()
@@ -15,8 +13,6 @@ export default function App()
                 <Routes>
                     <Route path="/" element={<Users />} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<SignUP />} />
-                    <Route path="/recovery" element={<Recovery />} />
                 </Routes>
             </BrowserRouter>
         </ReduxProvider>
--- src/components/Signup/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as SignupBox } from './Box';
-export { default as SignupForm } from './Form';
-export { default as SignupButton } from './Button';
